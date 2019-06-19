bcfaefd60219a2c3192e5fb8fa9c5c7e763c6411
Reyes Jimenez Adriana <boogiebasace@gmail.com>
2019-06-19 12:03:43 -0500

    Pase funciones puras al cipher y mejore el css

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,30 @@
 window.cipher = {
+  encode: (string,off)=>{//Declaro la funcion encode que recibe como parametros string (el texto) y off (el offset)
+    let res="";//Declaro el let res(resultado) que contendra el texto codificado(encriptado)
+    for(let i=0;i<string.length;i++){
+      let asci=string.charCodeAt(i);
+      if(asci>=65 && asci<=90){
+        res=res+String.fromCharCode((asci-65+off)%26+65);
+      }else{
+        res=res+String.fromCharCode(asci);
+      }
+    }
+    return res;//retorno res
+
+  }
+  ,decode: (string,off)=>{//Declaro el metodo decode que recibe como parametro string(el texto) y off ( el offset)
+    let res="";//Declaro el let res que contendra el texto decodificado(desencriptado)
+    for(let i=0;i<string.length;i++){
+      let asci=string.charCodeAt(i);
+      if(asci>=65&&asci<=90){
+        res=res+String.fromCharCode((asci+65-off)%26+65);//Voy concatenando los caracteres para crear la cadena res (resultado)
+      }else{
+        res=res+String.fromCharCode(asci);
+      }
+    }
+  return res;//retorno res
+  }
+
+
   // ... 
 };
--- src/index.js
+++ src/index.js
@@ -5,34 +5,53 @@ const enviar=document.getElementById('enviar');
 const encriptar=document.getElementById('encriptar');
 const output=document.getElementById('output');
 const desencriptar=document.getElementById('desencriptar');
+const butoff=document.getElementById("butoff");
+const centeroff=document.getElementById("centeroff");
+let offset;
+// Se almacena el numero que ingrese el usuario cuando apriete el boton butoff
+
 enviar.addEventListener('click',()=>{
   const nombre=document.getElementById('nombre').value;
-  center1.innerHTML=`<br>Bienvenido, ${nombre}`;
+  center1.innerHTML=`<br>Bienvenid@, ${nombre}`;
   center2.classList.add('ocultar');
-  center3.classList.remove('ocultar');
+  centeroff.classList.remove('ocultar');
 
 });
+
+//A offset se le asigna el valor entero de lo que se ingreso en input off 
+//luego hace que desaparezca el centeroff y que aparezca center3
+butoff.addEventListener('click',()=>{
+  offset=parseInt(document.getElementById('inputoff').value);
+  centeroff.classList.add("ocultar");
+  center3.classList.remove("ocultar");
+});
+
 encriptar.addEventListener('click',()=>{
   output.value="";
   let cadena=document.getElementById('input').value;
-  for(let i=0;i<cadena.length;i++){
+  /*for(let i=0;i<cadena.length;i++){
     let asci=cadena.charCodeAt(i);
-    if(asci>=65&&asci<=90){
-      output.value=output.value+String.fromCharCode((asci-65+33)%26+65);
+    if(asci>=65 && asci<=90){
+      output.value=output.value+String.fromCharCode((asci-65+offset)%26+65);
     }else{
       output.value=output.value+String.fromCharCode(asci);
     }
-  }
+  }*/
+  let salida=window.cipher.encode(cadena,offset);//llamo a la funcion encode y le doy como  parametro cadena y  offset y lo que retorna lo guardo en salida
+  output.value=salida;//Y al valor del output le asigno lo que tiene salida
 });
 desencriptar.addEventListener('click',()=>{
   output.value="";
   let cadena=document.getElementById('input').value;
-  for(let i=0;i<cadena.length;i++){
+  /*for(let i=0;i<cadena.length;i++){
     let asci=cadena.charCodeAt(i);
     if(asci>=65&&asci<=90){
-      output.value=output.value+String.fromCharCode((asci+65-33)%26+65);
+      output.value=output.value+String.fromCharCode((asci+65-offset)%26+65);
     }else{
       output.value=output.value+String.fromCharCode(asci);
     }
   }
+  */
+ let salida=window.cipher.decode(cadena,offset);
+ output.value=salida;
 });
