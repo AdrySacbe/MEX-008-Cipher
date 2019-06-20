e482bdf5fc03672f757df033ad08a64c025d7d8c
Reyes Jimenez Adriana <boogiebasace@gmail.com>
2019-06-20 11:59:42 -0500

    Puse las instrucciones cambie color

--- src/cipher.js
+++ src/cipher.js
@@ -1,10 +1,11 @@
 window.cipher = {
-  encode: (string,off)=>{//Declaro la funcion encode que recibe como parametros string (el texto) y off (el offset)
+  encode: (string, off)=>{//Declaro la funcion encode que recibe como parametros string (el texto) y off (el offset)
     let res="";//Declaro el let res(resultado) que contendra el texto codificado(encriptado)
-    for(let i=0;i<string.length;i++){
-      let asci=string.charCodeAt(i);
-      if(asci>=65 && asci<=90){
+    for(let i=0;i<string.length;i++){//Creo un ciclo i es igual a 0 hasta el tamaño de la cadena (string.legth)
+      let asci=string.charCodeAt(i);//creo una variable asci que va a contener un numero el que tiene string
+      if(asci>=65 && asci<=90){//si este asci es una letra mayuscula a mi variable res le sumo esa letra mayuscula
         res=res+String.fromCharCode((asci-65+off)%26+65);
+      
       }else{
         res=res+String.fromCharCode(asci);
       }
@@ -12,7 +13,7 @@ window.cipher = {
     return res;//retorno res
 
   }
-  ,decode: (string,off)=>{//Declaro el metodo decode que recibe como parametro string(el texto) y off ( el offset)
+  ,decode: (string, off)=>{//Declaro el metodo decode que recibe como parametro string(el texto) y off ( el offset)
     let res="";//Declaro el let res que contendra el texto decodificado(desencriptado)
     for(let i=0;i<string.length;i++){
       let asci=string.charCodeAt(i);
